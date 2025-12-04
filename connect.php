1908e39b142e616616d071f20b4c198df9696ced
Avijit Halder Joy <avijithalderjoy1@gmail.com>
2025-12-05 00:59:08 +0600

    link with alart page

--- connect.php
+++ connect.php
@@ -10,7 +10,8 @@ $con = mysqli_connect($host,$user,$password,$db);
 if(!$con){
     echo"Connection Failed";
 }else{
-    echo"Connection Successful";
+    header("Location: success.html");
+        exit();
 }
 
 ?>
